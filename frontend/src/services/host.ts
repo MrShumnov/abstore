40c89d2d857fa3d027783ac4d3734143f34c43fa
Петр Шумнов <mr.shumnov@gmail.com>
2023-02-27 20:41:19 +0300

    http2 configured

--- frontend/src/services/host.ts
+++ frontend/src/services/host.ts
@@ -1,3 +1,3 @@
-export const API_URL = "https://localhost:7264/api/"
+// export const API_URL = "https://localhost:7264/api/"
 // export const API_URL = "/api/v1/";
-// export const API_URL = process.env.BACKEND_SERVER;
+export const API_URL = process.env.REACT_APP_BACKEND_SERVER;
